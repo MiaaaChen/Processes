c7e4c70b028c3cbc459b43456a9452c6b3c60bb6
MiaaaChen <huiyingmia@gmail.com>
2025-04-17 18:37:41 +0800

    fix: use application/json content-type for Bonsai Elasticsearch OSS

--- backend/src/config/elasticsearch.ts
+++ backend/src/config/elasticsearch.ts
@@ -1,15 +1,17 @@
-// @src/config/elasticsearch.ts
 import { Client } from '@elastic/elasticsearch';
 
 const client = new Client({
-    node: process.env.ELASTICSEARCH_URL || 'http://localhost:9200',
-    auth:
-        process.env.ELASTICSEARCH_USERNAME && process.env.ELASTICSEARCH_PASSWORD
-            ? {
-                username: process.env.ELASTICSEARCH_USERNAME,
-                password: process.env.ELASTICSEARCH_PASSWORD,
-            }
-            : undefined,
+    node: process.env.ELASTICSEARCH_URL,
+    auth: process.env.ELASTICSEARCH_USERNAME && process.env.ELASTICSEARCH_PASSWORD
+        ? {
+            username: process.env.ELASTICSEARCH_USERNAME,
+            password: process.env.ELASTICSEARCH_PASSWORD,
+        }
+        : undefined,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
 });
 
 export default client;
